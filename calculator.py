5fe179b212be41915d771863cb6675d2ce58ee1d
reyhan-r1 <rahmanr@ufl.edu>
2025-11-12 15:40:31 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -6,16 +6,8 @@ calculator.py
 
 One function per operation, in order.
 """
-import math
-
 # First example
-def square_root(a):
-    if a < 0:
-        raise ValueError
-    return math.sqrt(a)
-def hypotenuse(a, b):
-    return math.hypot(a, b)
-def add(a, b): 
+def add(a, b):
     return a + b
 
 def sub(a,b):
@@ -40,25 +32,4 @@ def log(a,b):
 def exp(a,b):
     return a ** b
 
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if a == 0: raise ZeroDivisionError
-    else:
-        return b /a
-
-def log(a, b):
-    if b < 1: raise ValueError
-    else:
-        return math.log(a, b)
-
-def exp(a, b):
-    return a ** b
 
