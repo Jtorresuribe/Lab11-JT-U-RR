810af9809c9cf7f6c3a80327b6f18eb98bb45a20
Jtorresuribe <jtorresuribe@ufl.edu>
2025-11-12 15:36:09 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -36,29 +36,6 @@ def log(a,b):
     else:
         return math.log(a,b)
 
-
 def exp(a,b):
-    return a ** b
-
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
-
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    if a == 0: raise ZeroDivisionError
-    else:
-        return b /a
-
-def log(a, b):
-    if b < 1: raise ValueError
-    else:
-        return math.log(a, b)
-
-def exp(a, b):
     return a ** b
 
